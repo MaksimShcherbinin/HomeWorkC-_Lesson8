c8f10bfb2b02d8925fa26521afb494e555c7fe4f
Maksim <maksim6erbinin@mail.ru>
2022-07-21 00:03:27 +0300

    Added task 58

---
+++ Task_58/Program.cs
@@ -0,0 +1,49 @@
+﻿int[,] CreateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(100);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MultiplicationMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] result = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = matrix1[i, j] * matrix2[i, j];
+        }
+    }
+    return result;
+}
+
+Console.Write("Введите кол-во строк массивов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов массивов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix1 = CreateMatrix(m, n);
+int[,] matrix2 = CreateMatrix(m, n);
+
+PrintMatrix(matrix1);
+PrintMatrix(matrix2);
+PrintMatrix(MultiplicationMatrix(matrix1, matrix2));
